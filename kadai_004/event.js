c765fbbebc4345c295e6ddbae12aa24e721238b6
rikako-on <139257224+rikako-on@users.noreply.github.com>
2023-08-17 00:12:33 +0900

    event.jsをアップロード

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,1 +1,8 @@
-enent.js
+$(window).on('load scroll', function(e) {
+  if(e.type === 'load'){
+    console.log('loadイベントが発生しました');
+  }
+  if(e.type === 'scroll'){
+    console.log('scrollイベントが発生しました');
+  }
+});
