ed79e99fff871022e64d76ec24453f68c5e556ee
rikako-on <139257224+rikako-on@users.noreply.github.com>
2023-08-20 01:00:48 +0900

    summary.jsをアップロード

--- kadai_010/summary.js
+++ kadai_010/summary.js
@@ -1,1 +1,14 @@
-summary.js
+$(function() {
+  $('#change-color').on('click', function() {
+    $('#target').css('color', 'red');
+  });
+  $('#change-text').on('click', function() {
+    $('#target').text('Hello!!');
+  });
+  $('#fade-out').on('click', function() {
+    $('#target').fadeOut();
+  });
+  $('#fade-in').on('click', function() {
+    $('#target').fadeIn();
+  });
+});
